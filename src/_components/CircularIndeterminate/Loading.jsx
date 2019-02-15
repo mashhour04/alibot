d0b4f2302dc62a83c213325beb04b80b5034e116
Ahmed Mashhour <mpghknown@gmail.com>
2019-02-15 18:53:32 +0200

    adding loading files

---
+++ src/_components/CircularIndeterminate/Loading.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+  position: {
+    center: 1,
+    margin: 'auto'
+  }
+});
+
+function CircularIndeterminate(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.position}>
+      <CircularProgress className={classes.progress} />
+    </div>
+  );
+}
+
+CircularIndeterminate.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CircularIndeterminate);
