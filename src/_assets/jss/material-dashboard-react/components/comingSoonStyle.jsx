a5a63a2e0b3cf0900e8846c0d0f877ab6069779c
Ahmed Mashhour <mpghknown@gmail.com>
2019-02-16 19:20:30 +0200

    adding bookingsTable page

---
+++ src/_assets/jss/material-dashboard-react/components/comingSoonStyle.jsx
@@ -0,0 +1,23 @@
+const comingSoonStyle = {
+    background: {
+        image: '/images/background-image.png',
+        textColor: '#fff',
+        overlay: {
+            color: '#000',
+            opacity: '.5'
+        }
+    },
+    logo: {
+      background: "transparent",
+      boxShadow: "none"
+    },
+    title:{
+        text: 'Deals Creating'
+    }, 
+    links: {
+        
+    }
+  };
+  
+  export default comingSoonStyle;
+  
---
+++ src/_views/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import UnderConstruction from 'react-under-construction';
+import 'react-under-construction/build/css/index.css';
+import withStyles from "@material-ui/core/styles/withStyles";
+ 
+import comingSoonStyle from "../../_assets/jss/material-dashboard-react/components/comingSoonStyle";
+import GridContainer from '../../_components/Grid/GridContainer';
+
+function ComingSoon({ ...props }) {
+  
+    return (
+      <GridContainer>
+
+      </GridContainer>
+    );
+}
+
+export default withStyles(comingSoonStyle)(ComingSoon);
