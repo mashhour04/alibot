29c0c831763da7e6f8dbd556674537bdb6973b8a
Ahmed Mashhour <mpghknown@gmail.com>
2019-07-19 04:28:51 +0200

    redux devtools

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import DevTools from 'redux-devtools';
+// import DevTools from 'redux-devtools';
 import rootReducer from '../_reducers';
 const initialState = {};
 
 const middlware = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
 
 const store = createStore(rootReducer, initialState, composeEnhancers(
     applyMiddleware(...middlware)
