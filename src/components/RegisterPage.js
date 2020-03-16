5dbd89b1a197e59ae568249476576c2a43bbbcff
Ahmed Mashhour <mashhou04@gmail.com>
2020-03-16 18:40:17 +0200

    lorem epsum

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -120,7 +120,7 @@ class RegisterPage extends Component {
                         <Card>
                             <CardBody>
                                 <CardTitle>register as a vendor of your choice in Alibot</CardTitle>
-                                <CardSubtitle>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</CardSubtitle>
+                                <CardSubtitle>Spoon.ai is the virtual assistant and hub for both restaurant owners and their customers where for the customers it shows them different restaurants to reserve a table or many tables at time they want</CardSubtitle>
 
                                 <div role="application" className="wizard clearfix vertical" id="steps-uid-1">
                                     <div className="steps clearfix">
